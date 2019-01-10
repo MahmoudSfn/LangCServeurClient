98c6c6e28ee9d995bafaae0afc41cee2bb010da0
MahmoudSfn <35277201+MahmoudSfn@users.noreply.github.com>
2019-01-10 14:14:02 +0100

    Update client.c

--- client.c
+++ client.c
@@ -1,10 +1,3 @@
-/*
-    Usage :
-        1) Compile: gcc -o client client.c 
-        2) Run : ./server (machine) (PortNumber)
-                 exemple :  ./server Mahs 6543 
-*/
-
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -17,6 +10,8 @@
 #include <signal.h>
 #include <string.h>
 
+#define MAXTEXT 100
+
 void write_header(int sock, char * username) {
   int loglen = strlen(username);
   write(sock, &loglen, 1);
@@ -25,68 +20,76 @@ void write_header(int sock, char * username) {
 
 int main(int argc, char * argv[])
 {
-    /*Declaration des variables*/
-    int sock;
-    struct hostent * hote;
-    int port = 6543;
-    char *username;
-    struct sockaddr_in adr;
-    int pidFils;
+  int sock;
+  int port = 6543;
+  int pidFils;
+  char *username;
+  struct hostent * hote;
+  struct sockaddr_in adr;
+  
+  FILE *FileBat = fopen("bateau.txt", "w+");
+  fprintf(FileBat, "%s\n",argv[1]);
 
-    char c;
-    /***************************/
+  printf("%s\n", argv[1]);
+  
 
-    if (argc!=3)
+  if (argc!=2)
+  {
+    fprintf(stderr,"Usage : %s name,vitesse,direction\n", argv[0]);
+    exit(1);
+  }
+
+  if ((sock=socket(AF_INET, SOCK_STREAM, 0)) ==-1)
+  {
+    perror("socket rendez-vous");
+    exit(1);
+  }
+  
+  
+
+  hote=gethostbyname("Mahs");
+  port=atoi("6543");
+  username = getlogin(); /*pb : signature not conform to man doc.*/
+  printf("User: %s - %d; Machine: Mahs\n", username, geteuid());
+
+  adr.sin_family=AF_INET;
+  adr.sin_port=htons(port);
+  bcopy(hote->h_addr, &adr.sin_addr.s_addr, hote->h_length);
+
+  if (connect(sock, (struct sockaddr *)&adr, sizeof(adr))==-1)
+  {
+    perror("connect");
+    exit(1);
+  }
+
+  char c;
+  switch(pidFils=fork()) {
+
+  case -1:
+    perror("fork");
+    exit(1);
+  case 0:
+    do
     {
-        fprintf(stderr,"Usage : %s machine port-number\n", argv[0]);
-        exit(1);
+      c = EOF;
+      read(sock, &c, 1);
+      putchar(c);
     }
-
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    while (c!=EOF);
+    fprintf(stderr,"Cote client: fin fils\n");
+    break;
+  default:
+    /* First message sends the name of talking user */
+    write_header(sock, username);
+    do
     {
-        perror("socket rendez-vous");
-        exit(1);
+      c=getchar();
+      write(sock, &c, 1);
     }
+    while (c!=EOF);
 
-    hote = gethostbyname(argv[1]);
-    port = atoi(argv[2]);
-    username = getlogin();
-
-    printf("User: %s - %d; Machine: %s\n", username, geteuid(), argv[1]);
-
-    adr.sin_family=AF_INET;
-    adr.sin_port=htons(port);
-    bcopy(hote->h_addr, &adr.sin_addr.s_addr, hote->h_length);
-
-    if (connect(sock, (struct sockaddr *)&adr, sizeof(adr))==-1)
-    {
-        perror("connect");
-        exit(1);
-    }
-
-    switch(pidFils=fork()) 
-    {
-        case -1:
-            perror("fork");
-            exit(-1);
-        case 0:
-            do {
-                c = EOF; //EndOfFile
-                read(sock, &c, 1);
-                putchar(c);
-            } while(c != EOF);
-            fprintf(stderr, "Cote client: fin fils\n");
-            break;
-        default:
-            /* First message sends the name of talking user */
-            write_header(sock, username);
-
-            do {
-                c = getchar();
-                write(sock, &c, 1);
-            } while (c != EOF);
-            kill(pidFils, SIGTERM);
-            fprintf(stderr, "Cote client: fin pere\n");
-    }
-    return 0;
+    kill(pidFils,SIGTERM);
+    fprintf(stderr,"Cote client: fin pere\n");
+  }
+  return 0;
 }
